088772a2d599a5e70111e3160ffbb66c8f11c856
David Tanzer <david@davidtanzer.net>
2023-01-04 08:05:48 +0100

    Added integration tests for nesting blocks
    Nesting multi-line blocks is the feature where my previous two
    attempts for implementing a parser failed (with parsing updates
    parially being very difficult to implement). Because of that, I now
    want to quickly come to the point where I can test that. The three
    integration tests in NestingBlocks.spec.ts are my first guiding
    tests to get there.
    
    Later, there will also be tests for parsing updates partially with
    those nested blocks, and for even deeper nesting of blocks and
    inlines.

---
+++ test/integration/NestingBlocks.spec.ts
@@ -0,0 +1,55 @@
+import { Marmdown } from "$markdown/Marmdown"
+import { MfMDialect } from "$markdown/MfMDialect"
+import { html } from "./html"
+import { sanitized } from "./sanitize"
+
+describe('nesting blocks', () => {
+	const md = new Marmdown(new MfMDialect())
+	test.skip('blockquote > p', () => {
+		md.textContent = sanitized`
+			> Sphinx of black quartz,
+			> judge my vow.
+			>
+			> And another paragraph`
+
+		expect(html(md)).toEqual(sanitized`
+			<blockquote>
+			<p>Sphinx of black quartz, judge my vow.</p>
+			<p>And another paragraph
+			</blockquote>`)
+	})
+	test.skip('aside > blockquote > p', () => {
+		md.textContent = sanitized`
+			^ > Sphinx of black quartz,
+			^ > judge my vow.
+			^
+			^ > And another paragraph`
+
+		expect(html(md)).toEqual(sanitized`
+			<aside>
+			<blockquote>
+			<p>Sphinx of black quartz, judge my vow.</p>
+			</blockquote>
+			<blockquote>
+			<p>And another paragraph
+			</blockquote>
+			</aside>`)
+	})
+	test.skip('blockquote > ul > li > p', () => {
+		md.textContent = sanitized`
+			> * Sphinx of black quartz,
+			> * judge my vow.`
+
+		expect(html(md)).toEqual(sanitized`
+			<blockquote>
+			<ul>
+			<li>
+			<p>Sphinx of black quartz,</p>
+			</li>
+			<li>
+			<p>judge my vow.</p>
+			</li>
+			</ul>
+			</blockquote>`)
+	})
+})
