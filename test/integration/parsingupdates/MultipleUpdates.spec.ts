6355b89e20afbde4f032bdabfde219dc7d0ec343
David Tanzer <david@davidtanzer.net>
2023-09-05 07:41:22 +0200

    Added test for re-rendering multiple updates

--- test/integration/parsingupdates/MultipleUpdates.spec.ts
+++ test/integration/parsingupdates/MultipleUpdates.spec.ts
@@ -14,6 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+import { Element } from "src/element/Element"
 import { Marmdown } from "../../../src/Marmdown"
 import { MfMDialect } from "../../../src/MfMDialect"
 import { html } from "../html"
@@ -34,8 +35,27 @@ describe('MfM: Parsing updates - Multiple Updates', () => {
 			<h1>The updated heading content.</h1>
 			<p>And some more updated paragraph content
 			with two lines.</p>`)
-
 	})
 
-	test.skip('parsing two updates, in bold and italic of two paragraphs', () => {})
+	test('parsing multiple updates at the end of a paragraph', () => {
+		md.textContent = sanitized`foo`
+
+		let childrenChangedOn: string[] = []
+		const attachListeners = (e: Element<unknown, unknown, unknown, unknown>) => {
+			e.content?.forEach((c: any, i) => {
+				c.childrenChanged = () => childrenChangedOn.push(`${c.type}: ${i}`)
+				attachListeners(c)
+			})
+		}
+		attachListeners(md.document!)
+
+		md.update({ text: ' ', rangeOffset: 3, rangeLength: 0 }, () => 'dummy')
+		attachListeners(md.document!)
+		md.update({ text: 'b', rangeOffset: 4, rangeLength: 0 }, () => 'dummy')
+		md.update({ text: 'a', rangeOffset: 5, rangeLength: 0 }, () => 'dummy')
+		md.update({ text: 'r', rangeOffset: 6, rangeLength: 0 }, () => 'dummy')
+
+		expect(md.textContent).toEqual('foo bar')
+		expect(childrenChangedOn).toHaveLength(4)
+	})
 })
